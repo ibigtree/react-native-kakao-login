0a51590b5feb49004435b5644d236eb9788ebd79
Daehan Kim <daehankim@ibigtree.kr>
2021-11-05 16:24:32 +0900

    Type 진짜 제대로 수정

--- index.d.ts
+++ index.d.ts
@@ -2,10 +2,11 @@ export interface KakaoAuthTokenInfo {
     accessToken: string;
     refreshToken: string;
 }
-
 interface KakaoLogin {
     login(): Promise<KakaoAuthTokenInfo>;
     logout(): Promise<boolean>;
 }
+
+declare const KakaoLogin: KakaoLogin;
 
 export default KakaoLogin;
